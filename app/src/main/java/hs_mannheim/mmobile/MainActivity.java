8aee46ea0e5b25fbbb9e23a9cb10b2f84cce683c
Horst Schneider <h.schneider@hs-mannheim.de>
2016-05-18 14:19:21 +0200

    moves polling logic to separate class and refactors it a bit. Also adds some error handling logic.

--- app/src/main/java/hs_mannheim/mmobile/MainActivity.java
+++ app/src/main/java/hs_mannheim/mmobile/MainActivity.java
@@ -1,108 +1,32 @@
 package hs_mannheim.mmobile;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "Main";
-    private TextView mTv;
+public class MainActivity extends AppCompatActivity implements SwipeDetector.SwipeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
-
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        mTv = (TextView) findViewById(R.id.tv);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                boolean found = false;
-                while(!found) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    String result = new StringStore().read("cas_mmobile_swipe_data");
-                    if(!result.isEmpty()) {
-                        Handler refresh = new Handler(Looper.getMainLooper());
-                        refresh.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                mTv.setText("SWIPE");
-                            }
-                        });
-                        found = true;
-                        new StringStore().write("cas_mmobile_swipe_data", "");
-                    }
-                }
-            }
-        }).start();
+        SwipeDetector swipeDetector = new SwipeDetector();
+        swipeDetector.registerObserver(this);
+        swipeDetector.start();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onSwipeDetected() {
+        Toast.makeText(this, "SWIPE!", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/hs_mannheim/mmobile/SwipeDetector.java
@@ -0,0 +1,82 @@
+package hs_mannheim.mmobile;
+
+import android.database.Observable;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+/**
+ * Polls the SysPlace server for incoming swipes from the CAS MMobile app.
+ */
+public class SwipeDetector extends Observable<SwipeDetector.SwipeListener> {
+    private static final String KEY = "cas_mmobile_swipe_data";
+    private static final int FREQUENCY = 1000;
+
+    private final StringStore mStringStore;
+
+    public SwipeDetector() {
+        mStringStore = new StringStore();
+    }
+
+    /**
+     * Starts polling for incoming swipes at a given frequency.
+     */
+    public void start() {
+        new Thread(new LooperRunnable()).start();
+    }
+
+    /**
+     * Notifies all registered SwipeListeners when a swipe occurred.
+     */
+    private void notifyListeners() {
+        for (final SwipeListener listener : mObservers) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    listener.onSwipeDetected();
+                }
+            });
+        }
+    }
+
+    /**
+     * Runnable that implements the polling mechanism.
+     */
+    private class LooperRunnable implements Runnable {
+        private final String TAG = "SwipeDetector";
+
+        @Override
+        public void run() {
+            Looper.prepare();
+
+            boolean abort = false;
+
+            while (!abort) {
+                try {
+                    Thread.sleep(FREQUENCY);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, e.getMessage());
+                    abort = true;
+                }
+
+                String result = mStringStore.read(KEY);
+                Log.d(TAG, "Fetched result: " + result);
+
+                if (!result.isEmpty()) {
+                    notifyListeners();
+
+                    // reset swipe data
+                    mStringStore.write(KEY, "");
+                }
+            }
+        }
+    }
+
+    /**
+     * Implemented by listeners that want to be notified of CAS MMobile Swipe events.
+     */
+    public interface SwipeListener {
+        void onSwipeDetected();
+    }
+}
